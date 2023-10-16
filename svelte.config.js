c45c3499baa70d7ee9ddff5dcb97fb6c83f67ff8
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-10-16 14:40:36 -0300

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,5 +14,7 @@ const config = {
 		adapter: adapter()
 	}
 };
+
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 
 export default config;
