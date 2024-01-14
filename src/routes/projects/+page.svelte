c3f7aa6831819ad7277c4ccdf7a89d060067d8de
Juliano Lorenzato <julianolorenzato@gmail.com>
2024-01-14 13:22:05 -0300

    testing env

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { env } from '$env/dynamic/public'
+    const a = env.PUBLIC_GITHUB_API_URL
+</script>
+
+<h1>Here are my projects ${a}</h1>
---
+++ src/routes/projects/+page.ts
@@ -0,0 +1,6 @@
+// import type { PageLoad } from './$types'
+// import axios from 'axios'
+
+// export const load: PageLoad = async () => {
+// 	await axios.get('')
+// }
